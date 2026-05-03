2c407736243f58fabb55fd9d988042ab15bd8193
anmolbisht2308 <anmolbisht7@gmail.com>
2026-05-03 14:03:27 +0530

    chore: cleanup debug logging

--- src/server.ts
+++ src/server.ts
@@ -1,55 +1,33 @@
 import "dotenv/config";
 import http from "node:http";
-import fs from "node:fs";
-
-fs.writeSync(2, "SERVER STARTING UP...\n");
-setInterval(() => fs.writeSync(2, "HEARTBEAT...\n"), 500);
-
-process.on("uncaughtException", (err) => {
-  fs.writeSync(2, "UNCAUGHT EXCEPTION: " + String(err) + "\n" + (err?.stack || "") + "\n");
-  setTimeout(() => process.exit(1), 2000);
-});
-
-process.on("unhandledRejection", (reason, promise) => {
-  fs.writeSync(2, "UNHANDLED REJECTION: " + String(reason) + "\n");
-  setTimeout(() => process.exit(1), 2000);
-});
-
 import app from "./serverApp";
 import { connectMongo } from "./lib/mongoose";
 import { connectRedis } from "./lib/redis";
 import { logger } from "./lib/logger";
 
+process.on("uncaughtException", (err) => {
+  logger.error("UNCAUGHT EXCEPTION", { err });
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("UNHANDLED REJECTION", { reason });
+  process.exit(1);
+});
+
 const port = Number(process.env.PORT ?? 5000);
 
 async function bootstrap(): Promise<void> {
-  fs.writeSync(2, `BOOTSTRAP RUNNING. MONGODB_URI exists: ${!!process.env.MONGODB_URI}, REDIS_URL exists: ${!!process.env.REDIS_URL}\n`);
-  
-  try {
-    await connectMongo();
-    fs.writeSync(2, "Mongo connected successfully.\n");
-  } catch (e) {
-    fs.writeSync(2, "MONGO CONNECTION THREW AN ERROR: " + String(e) + "\n");
-    throw e;
-  }
-  
-  try {
-    await connectRedis();
-    fs.writeSync(2, "Redis connected successfully.\n");
-  } catch (e) {
-    fs.writeSync(2, "REDIS CONNECTION THREW AN ERROR: " + String(e) + "\n");
-    throw e;
-  }
+  await connectMongo();
+  await connectRedis();
 
   const server = http.createServer(app);
-  server.listen(port, "0.0.0.0", () => {
-    fs.writeSync(2, `SERVER SUCCESSFULLY LISTENING ON PORT ${port}\n`);
+  server.listen(port, () => {
     logger.info("API server listening", { port });
   });
 }
 
 bootstrap().catch((err: unknown) => {
-  fs.writeSync(2, "Fatal bootstrap error: " + String(err) + "\n");
   logger.error("Fatal bootstrap error", { err });
-  setTimeout(() => process.exit(1), 2000);
+  process.exit(1);
 });
